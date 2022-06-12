ea7c2970d22e5386e9725ee8daebc510a2a5c93f
syaddadh1211 <syaddad.bahalwan@realworld-systems.com>
2022-06-12 23:57:29 +0700

    update promises.js

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (marah) => {
+  try {
+    const arrNgamuk = (await promiseTheaterIXX()).concat(
+      await promiseTheaterVGC()
+    );
+    return arrNgamuk.filter((record) => record.hasil === marah).length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
